7ac492e16e029a97f55cca65fa19ceafcff4c88f
Andreas Müller <taori@users.noreply.github.com>
2018-12-09 21:31:28 +0100

    Control library fix

--- templateSources/WpfApplication/Company.Desktop.Framework.Controls/Properties/AssemblyInfo.cs
+++ templateSources/WpfApplication/Company.Desktop.Framework.Controls/Properties/AssemblyInfo.cs
@@ -4,12 +4,8 @@ using System.Windows.Markup;
 
 [assembly: ComVisible(false)]
 [assembly:ThemeInfo(
-    ResourceDictionaryLocation.None, //Speicherort der designspezifischen Ressourcenwörterbücher
-                             //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-                             // oder in den Anwendungsressourcen-Wörterbüchern nicht gefunden werden kann.)
-    ResourceDictionaryLocation.SourceAssembly //Speicherort des generischen Ressourcenwörterbuchs
-                                      //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-                                      // designspezifischen Ressourcenwörterbuch nicht gefunden werden kann.)
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly
 )]
 [assembly: XmlnsPrefix("http://schemas.localcontrols.com/winfx/2006/xaml/presentation", "framework")]
 [assembly: XmlnsDefinition("http://schemas.localcontrols.com/winfx/2006/xaml/presentation", "Company.Desktop.Framework.Controls")]
