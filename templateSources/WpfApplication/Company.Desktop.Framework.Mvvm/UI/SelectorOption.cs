11b6437a1d54113c530cdfae4fad7675bb94a104
Andreas Müller <taori@users.noreply.github.com>
2020-01-04 02:45:34 +0100

    wip #4

---
+++ templateSources/WpfApplication/Company.Desktop.Framework.Mvvm/UI/SelectorOption.cs
@@ -0,0 +1,25 @@
+﻿namespace Company.Desktop.Framework.Mvvm.UI
+{
+	public class SelectorOption<TValue>
+	{
+		public SelectorOption(TValue value, string description)
+		{
+			Value = value;
+			Description = description;
+		}
+
+		public TValue Value { get; set; }
+
+		public string Description { get; set; }
+
+		public static implicit operator SelectorOption<TValue>((string text, TValue value) source)
+		{
+			return new SelectorOption<TValue>(source.value, source.text);
+		}
+
+		public static implicit operator SelectorOption<TValue>((TValue value, string text) source)
+		{
+			return new SelectorOption<TValue>(source.value, source.text);
+		}
+	}
+}
