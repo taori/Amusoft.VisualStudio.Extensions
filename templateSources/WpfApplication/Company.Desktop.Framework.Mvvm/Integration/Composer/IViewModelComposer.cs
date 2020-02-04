a759d3610a1425c307290974b5944bd9767fecc4
Andreas Müller <taori@users.noreply.github.com>
2020-02-04 22:32:11 +0100

    wip

---
+++ templateSources/WpfApplication/Company.Desktop.Framework.Mvvm/Integration/Composer/IViewModelComposer.cs
@@ -0,0 +1,16 @@
+﻿namespace Company.Desktop.Framework.Mvvm.Integration.Composer
+{
+	public interface IViewModelComposer
+	{
+		T Compose<T>();
+	}
+
+	public class ViewModelComposer : IViewModelComposer
+	{
+		/// <inheritdoc />
+		public T Compose<T>()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
