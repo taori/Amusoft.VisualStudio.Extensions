1de0d7aea0efae2970ff3b50addb6fd753a692fd
Andreas Müller <taori@users.noreply.github.com>
2020-02-04 22:38:56 +0100

    fix of build error in release mode

--- templateSources/EfDataLayer/Model.EntityFramework/Utility/DefaultDataContextFactory.cs
+++ templateSources/EfDataLayer/Model.EntityFramework/Utility/DefaultDataContextFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging.Debug;
 
 namespace Model.EntityFramework.Utility
